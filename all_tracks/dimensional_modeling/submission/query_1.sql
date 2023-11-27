8e01b6718b9b74b899e9bba9ebbde521d03dbed6
AmiPandya <amipandya2611@gmail.com>
2023-11-26 23:15:11 -0800

    Update query_1.sql

---
+++ all_tracks/dimensional_modeling/submission/query_1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE
+  amipandya.actors (
+    actor VARCHAR,
+    actorid VARCHAR,
+    films ARRAY (
+    ROW (
+    film VARCHAR,
+    votes DOUBLE,
+    rating DOUBLE,
+    film_id VARCHAR
+    )
+    ),
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    current_year INTEGER
+  )
+  WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
